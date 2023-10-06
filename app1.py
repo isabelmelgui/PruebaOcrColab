15f7cb1a75d3e1a39bcc73ef9899cc56b0ebd3ce
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-10-05 19:05:02 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,25 +7,25 @@ from PIL import Image
 
 st.title("Reconocimiento óptico de Caracteres")
 
-img_file_buffer = st.file_uploader("Choose a jpg file")
+img_file_buffer2 = st.file_uploader("Choose a jpg file")
 
 with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+      filtro2 = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
 
-if img_file_buffer is not None:
+if img_file_buffer2 is not None:
     # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    bytes_data2 = img_file_buffer2.getvalue()
+    cv2_img2 = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
     
-    if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
+    if filtro2 == 'Con Filtro':
+         cv2_img2=cv2.bitwise_not(cv2_img)
     else:
-         cv2_img= cv2_img
+         cv2_img2 = cv2_img2
     
         
-    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
+    img_rgb2 = cv2.cvtColor(cv2_img2, cv2.COLOR_BGR2RGB)
+    text=pytesseract.image_to_string(img_rgb2)
     st.write(text) 
 
 
